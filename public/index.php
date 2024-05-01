7b6a3cd58472c672e0df0a1f71eb6e0ee17d6059
ssamuel645 <ssamuel645@gmail.com>
2024-05-01 15:33:40 -0300

    feature:create form

--- public/index.php
+++ public/index.php
@@ -4,15 +4,15 @@ require __DIR__ . '/../bootstrap.php';
 // Esta url será o valor para navegação
 $url = isset($_GET['url']) ? $_GET['url'] : '/';
 
-if($url === '/') {
+if ($url === '/') {
     //include '../templates/list.php';
     require TEMPLATES . '/list.phtml';
 }
 
-if($url === '/produto') {
-    $id = isset($_GET['id']) ? $_GET['id'] :'';
+if ($url === '/produto') {
+    $id = isset($_GET['id']) ? $_GET['id'] : '';
 
-    if($id) {
+    if ($id) {
         /* foreach(PRODUCTS as $currentProduct) {
             if($currentProduct['id'] == $id) {
                 $product = $currentProduct;
@@ -20,18 +20,27 @@ if($url === '/produto') {
             }
         } */
 
-        $product = array_filter(PRODUCTS, function($product) use ($id) {
+        $product = array_filter(PRODUCTS, function ($product) use ($id) {
             return $product['id'] == $id;
         });
-        
-        $product = current($product);
 
+        $product = current($product);
     }
 
-    if(!$id || !$product) {
+    if (!$id || !$product) {
         die('O produto selecionado não existe');
     }
 
     $title = $product['name'];
     require TEMPLATES . '/show.phtml';
+}
+
+if ($url === '/produto/novo') {
+    require TEMPLATES . '/create.phtml';
+}
+
+if ($url === '/produto/create') {
+    print '<pre>';
+    var_dump($_POST);
+    var_dump($_FILES);
 }
